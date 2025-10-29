ab068168720d35de9acad14e84fb385a71150445
Renan <renan.ferrer@hotmail.com>
2025-10-29 18:25:35 -0300

    ci

--- test/setupFlow.js
+++ test/setupFlow.js
@@ -1,10 +1,9 @@
 // test/setupFlow.js
 const flow = require('pactum-flow-plugin');
 
-flow.config.url = process.env.FLOW_URL || 'http://localhost:8080';
+flow.config.url = process.env.FLOW_URL || 'http://127.0.0.1:8080';
 flow.config.project = process.env.FLOW_PROJECT || 'nodejs-m24';
 flow.config.version = process.env.FLOW_VERSION || 'local';
-
 flow.config.overwrite = true;
 
 module.exports = flow;
